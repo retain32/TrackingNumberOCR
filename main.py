d91a221cf69cca55a6916c1ee43997552d48f507
retain32 <sethphillips97@gmail.com>
2019-02-28 16:31:53 -0500

    Cleaned up, moved image enhancement to an optional function as it seems to be unnecessary most of the time.

--- main.py
+++ main.py
@@ -5,29 +5,41 @@ from pytesseract.pytesseract import image_to_string
 import regex
 from PIL import Image, ImageEnhance, ImageFilter
 
-IMG_PATH = "usps_sample.jpg"
-T_CONT = 112  #Target contrast for image processing
+IMG_PATH = "usps_sample_2.jpg"
+T_CONT = 112  # Target contrast for image processing
 PYTESS_CONF = '-c tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyz#:;'
 
-im = Image.open(IMG_PATH)
-im = im.convert("RGBA") #Converts image (usually JPG) data to RGBA data for editing
-im_data = im.getdata()
 
-newimdata = []
-for pixel in im_data:
-    if pixel[0] < T_CONT or pixel[1] < T_CONT or pixel[2] < T_CONT:
-        newimdata.append(pixel)
-    else:
-        newimdata.append((255, 255, 255))
-im.putdata(newimdata)
+def main():
+    im = Image.open(IMG_PATH)
+    im = im.convert("RGBA")  # Converts image (usually JPG) data to RGBA data for editing
+    # im = enhance(im)
+    ocr_out_raw = pytesseract.image_to_string(im, config=PYTESS_CONF, lang='eng')
+    # print(ocr_out_raw)
 
-im = im.filter(ImageFilter.MedianFilter())  # Apply median filter
-enhancer = ImageEnhance.Contrast(im)
-im = enhancer.enhance(2)
-im = im.convert('1')  #Converts RGBA data to JPG
-# im.save(IMG_PATH.split('.')[0] + '_contrast.jpg')
-ocr_output = pytesseract.image_to_string(im, config=PYTESS_CONF, lang='eng')
-# print(ocr_output)
+    tracking_num = regex.search('(?:#:)([0-9\\s]+)', ocr_out_raw)[1]
+    tracking_num = ''.join(tracking_num.split())
+    print(tracking_num)
 
-sudhfasdjf = regex.search('(?:#:)([0-9\\s]+)', ocr_output)[1]
-print(sudhfasdjf)
+
+def enhance(im):
+    im_data = im.getdata()
+
+    newimdata = []
+    for pixel in im_data:
+        if pixel[0] < T_CONT or pixel[1] < T_CONT or pixel[2] < T_CONT:
+            newimdata.append(pixel)
+        else:
+            newimdata.append((255, 255, 255))
+    im.putdata(newimdata)
+
+    im = im.filter(ImageFilter.MedianFilter())  # Apply median filter
+    enhancer = ImageEnhance.Contrast(im)
+    im = enhancer.enhance(2)
+    im = im.convert('1')  # Converts RGBA data to JPG
+    im.save(IMG_PATH.split('.')[0] + '_contrast.jpg')
+    return im
+
+
+if __name__ == "__main__":
+    main()
