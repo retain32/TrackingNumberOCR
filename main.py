385bfcf102826b0e432d9b7ee28665c93f30aad1
retain32 <sethphillips97@gmail.com>
2019-03-02 01:10:14 -0500

    Functioning USPS api.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Authors: Jason Giroux, Seth Phillips
 # Uses tracking.py from LiterallyLarry. github.com/LiterallyLarry/USPS-Tracking-Python
 
-
+import tracking
 import pytesseract
 import regex
 import platform
@@ -12,19 +12,28 @@ from pathlib import Path
 from PIL import Image, ImageEnhance, ImageFilter
 
 IMG_PATH = "receipts"  # Directory containing receipt images
-T_CONT = 112  # Target contrast for image processing
 PYTESS_CONF = '-c tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyz#:;'
 
 if platform.system() == 'Windows':
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+    path_delimeter = '\\'
+else:
+    path_delimeter = '/'
 
 
 def main():
     pathlist = Path(IMG_PATH).glob('**/*.jpg')
     for path in pathlist:
         image_path = str(path)
-        print(image_path)
-        ocr(image_path)
+        print(image_path.split(path_delimeter)[1])
+        tracking_num = ocr(image_path)
+        if not tracking_num:
+            print('OCR failed, no tracking number found.')
+        else:
+            print(tracking_num)
+            print(tracking.usps_track(tracking_num))
+        print('\n')
+
 
 
 def ocr(image_path):
@@ -36,19 +45,21 @@ def ocr(image_path):
     try:
         tracking_num = regex.search('(?:#:)([0-9\\s]+)', ocr_out_raw)[1]
         tracking_num = ''.join(tracking_num.split())  # There's probably a more elegant way to do this.
-        print(tracking_num + '\n')
+        return tracking_num
 
     except TypeError:
-        print('OCR failed, no tracking number found.\n')
+        return
 
 
 def enhance(im):
+    t_cont = 112  # Target contrast for image processing
+
     im = im.convert("RGBA")  # Converts image (usually JPG) data to RGBA data for editing
     im_data = im.getdata()
 
     newimdata = []
     for pixel in im_data:
-        if pixel[0] < T_CONT or pixel[1] < T_CONT or pixel[2] < T_CONT:
+        if pixel[0] < t_cont or pixel[1] < t_cont or pixel[2] < t_cont:
             newimdata.append(pixel)
         else:
             newimdata.append((255, 255, 255))
--- tracking.py
+++ tracking.py
@@ -3,94 +3,55 @@
 # Tested on Python 3.4.2 running on Debian 8.7
 # https://github.com/LiterallyLarry/USPS-Tracking-Python
 #
-# You must provide your API key in config.json as 'api_key' before running this program! You can sign up for an API key here: https://www.usps.com/business/web-tools-apis/welcome.htm
+# You must provide your API key as 'api_key' before running this program! You can sign up for an API key here: https://www.usps.com/business/web-tools-apis/welcome.htm
 # Modified by Seth Phillips
 
 from urllib import request, parse
-from sys import argv
 from xml.etree import ElementTree
 import argparse, json, sys, os
 
 USPS_API_URL = "http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2";
 
-path = os.path.dirname(os.path.realpath(__file__))
+API_KEY = '941SIMPL7834'
 
-with open(os.path.join(path, "config.json")) as config_file:
-    config = json.load(config_file);
-    api_key = config.get("api_key");
+if not API_KEY:
+    sys.exit("Error: Could not find USPS API key");
 
-if not api_key:
-    sys.exit("Error: Could not find USPS API key in config.json!");
 
-parser = argparse.ArgumentParser(description='Tracks USPS numbers via Python.');
-
-parser.add_argument('tracking_numbers', metavar='TRACKING_NUMBER', type=str, nargs='*',
-                    help='a tracking number');
-parser.add_argument('-s', action='store_true', default=False,
-                    dest='show_tracking_number',
-                    help='Show tracking number in output');
-parser.add_argument('-n', action='store_false', default=True,
-                    dest='show_tracking_extended',
-                    help='Hide extended tracking information');
-parser.add_argument('-m', action='store_true', default=False,
-                    dest='show_minimal',
-                    help='Repress UI');
-
-def usps_track(numbers_list):
-    xml = "<TrackRequest USERID=\"%s\">" % api_key;
-    for track_id in numbers_list:
-        xml += "<TrackID ID=\"%s\"></TrackID>" % track_id;
+def submit_track_id(track_id):
+    xml = "<TrackRequest USERID=\"%s\">" % API_KEY;
+    xml += "<TrackID ID=\"%s\"></TrackID>" % track_id;
     xml += "</TrackRequest>";
-    target = "%s&%s" % (USPS_API_URL, parse.urlencode({ "XML" : xml }));
+    target = "%s&%s" % (USPS_API_URL, parse.urlencode({"XML": xml}));
     request_obj = request.urlopen(target);
     result = request_obj.read();
     request_obj.close();
     return result;
 
-if __name__ == "__main__":
-    args = parser.parse_args();
-    if args.tracking_numbers: # Arguments support multiple tracking numbers
-        track_ids = args.tracking_numbers;
-        #track_ids = argv[1:];
-    else:
-        #track_id = input(); # User input supports only a single number
-        track_id = input('Enter tracking numbers separated by spaces: '); # User input supports multiple tracking numbers split with spaces
-        if len(track_id) < 1:
-            exit(0);
-        track_ids = track_id.split(' ');
-        #track_ids = [ track_id ];
-    real = []
-    for id in track_ids:
-        if id[0] != '#':
-            real.append(id);
-    track_ids = real
-    track_xml = usps_track(track_ids);
-#    print(track_xml);
+
+def usps_track(track_id):
+    track_xml = submit_track_id(track_id);
+    # print(track_xml)
     track_result = ElementTree.ElementTree(ElementTree.fromstring(track_xml));
-    if not args.show_minimal:
-        print('OK!');
-    for result in track_result.findall('Description'):
-        print(result.text);
-#    for result in track_result.findall('.//TrackSummary'):
-#        print(result.text);
-    for number, result in enumerate(track_result.findall('.//TrackInfo')):
-        if args.show_tracking_number:
-            track_num = ' (%s)' % track_ids[number];
-        else:
-            track_num = ''
-        summary = result.find('TrackSummary');
-        if summary is None:
-            print('Error in XML!');
-            print(track_xml);
-        else:
-            if args.show_minimal:
-                print('%s' % summary.text);
-            else:
-                print('\nPackage #%d%s:\n %s' % (number+1,track_num,summary.text));
-            if args.show_tracking_extended:
-                details = result.findall('TrackDetail');
-                for number_2, detailed_result in enumerate(details):
-                    if number_2+1 == len(details):
-                        print('  └ %s' % detailed_result.text);
-                    else:
-                        print('  ├ %s' % detailed_result.text);
+
+    summary = track_result.findall('.//TrackSummary')
+    if summary:
+        # print('summary: %s' % summary[0].text)
+        return summary[0].text
+
+    error = track_result.findall('.//Description')
+    if error:
+        # print('error: %s' % error[0].text)
+        return error[0].text
+        # print('\n\nsummary: %s' % summary.text);
+        # print('\n\nerror: %s' % error.text);
+
+        # if summary is None:
+        #     print('Error in XML!');
+        #     print(track_xml);
+        # else:
+        #     print('\n\nmsg: %s' % summary.text);
+
+
+if __name__ == "__main__":
+    usps_track()
